ca2f3ba3bec30dd9ee7ca0553b2b69acad8b1174
Alex Carlson <alex-carlson@users.noreply.github.com>
2026-03-16 16:36:37 -0400

    allow freebeer.nyc domain

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 const allowedOrigins = [
     process.env.FRONTEND_URL || 'http://localhost:5173',
     'https://quizzems.com',
+    'https://freebeer.nyc',
     'https://www.quizzems.com',
     'http://localhost:3000',
     'http://localhost:5174',
